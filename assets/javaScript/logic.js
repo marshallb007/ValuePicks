720a5b0969abbfde820d622e9fb59c7b4f19fdf8
Ryan Stowe <ryanwstowe@gmail.com>
2018-04-01 18:05:36 -0400

    added navbar to contact and corrected the autopop-up modal

--- assets/javaScript/logic.js
+++ assets/javaScript/logic.js
@@ -9,31 +9,42 @@ var config = {
 firebase.initializeApp(config);
 
 // $('#modalLoginForm').on('show.bs.modal', function (event) {
-//   var button = $(event.relatedTarget); // Button that triggered the modal
-//   var recipient = button.data('whatever'); // Extract info from data-* attributes
-//   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-//   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-//   var modal = $(this);
-//   modal.find('.modal-title').text('New message to ' + recipient);
-//   modal.find('.modal-body input').val(recipient);
-//
+//   var button = $(event.modalLoginForm); // Button that triggered the modal
+// var recipient = button.data('whatever'); // Extract info from data-* attributes
+// If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+// Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+// var modal = $(this);
+// modal.find('.modal-title').text('New message to ' + recipient);
+// modal.find('.modal-body input').val(recipient);
+
 // });
 
 
 // $('#modalLoginForm').modal({
-  // keyboard: false
+// keyboard: false
 // });
 
 
-// $('#modalLoginForm').modal('toggle');
+//
 
 
-// $('#modalLoginForm').modal('show');
-//
-//
-// $('#modalLoginForm').modal('hide');
-//
-//
+$('#openSignIn').on('click', function  {
+  modal('show');
+  focus
+  $('#modalLoginForm')
+};
+
+
+$('#signInClose').on('click', function (event) {
+  focus('#modalLoginForm');
+.
+  modal('hide');
+  $('#modalLoginForm').modal('toggle');
+
+
+};
+
+
 // $('#modalLoginForm').on('hidden.bs.modal', function (event) {
 //   // do something...
 // });
