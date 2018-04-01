f6063cf909590ad35fb73854ecdda44b5bbed462
Ryan Stowe <ryanwstowe@gmail.com>
2018-04-01 17:27:02 -0400

    login and signup modals have been created, and can be opened but focus has not been set and they will only close on refres

--- assets/javaScript/logic.js
+++ assets/javaScript/logic.js
@@ -8,33 +8,35 @@ var config = {
 };
 firebase.initializeApp(config);
 
-$('#exampleModal').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget); // Button that triggered the modal
-  var recipient = button.data('whatever'); // Extract info from data-* attributes
-  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  var modal = $(this)
-  modal.find('.modal-title').text('New message to ' + recipient);
-  modal.find('.modal-body input').val(recipient);
+// $('#modalLoginForm').on('show.bs.modal', function (event) {
+//   var button = $(event.relatedTarget); // Button that triggered the modal
+//   var recipient = button.data('whatever'); // Extract info from data-* attributes
+//   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+//   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+//   var modal = $(this);
+//   modal.find('.modal-title').text('New message to ' + recipient);
+//   modal.find('.modal-body input').val(recipient);
+//
+// });
 
 
-});
+// $('#modalLoginForm').modal({
+  // keyboard: false
+// });
 
 
-$('#myModal').modal({
-  keyboard: false
-});
+// $('#modalLoginForm').modal('toggle');
 
 
-$('#myModal').modal('toggle');
+// $('#modalLoginForm').modal('show');
+//
+//
+// $('#modalLoginForm').modal('hide');
+//
+//
+// $('#modalLoginForm').on('hidden.bs.modal', function (event) {
+//   // do something...
+// });
 
 
-$('#myModal').modal('show');
 
-
-$('#myModal').modal('hide');
-
-
-$('#myModal').on('hidden.bs.modal', function (e) {
-  // do something...
-});
